d7fa690b114d626b67631862be170655424c28ef
themaing <olardi2@gmail.com>
2017-05-08 22:23:02 +0100

    update operations function

--- js/operations.js
+++ js/operations.js
@@ -1,16 +1,31 @@
-const operations = {
-    add : function (num1, num2) {
+function operations() {
+    function add(num1, num2) {
         return num1 + num2;
-    },
-    subtract : function (num1, num2) {
+    }
+    function subtract(num1, num2) {
         return num1 - num2;
-    },
-    multiply : function (num1, num2) {
+    }
+    function multiply(num1, num2) {
         return num1 * num2;
-    },
-    divide : function (num1, num2) {
+    }
+    function divide(num1, num2) {
         return num1 / num2;
     }
 }
-module.exports = operations;
+
+// const operations = {
+//     add : function (num1, num2) {
+//         return num1 + num2;
+//     },
+//     subtract : function (num1, num2) {
+//         return num1 - num2;
+//     },
+//     multiply : function (num1, num2) {
+//         return num1 * num2;
+//     },
+//     divide : function (num1, num2) {
+//         return num1 / num2;
+//     }
+// }
+// module.exports = operations;
 
